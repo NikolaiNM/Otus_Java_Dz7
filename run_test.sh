689b8959bbd67569788e83bd89cd3f1070cff968
Nick Malykhin <popneo2@gmail.com>
2024-11-14 22:05:24 +0300

    upd script

--- run_test.sh
+++ run_test.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# Проверка переменных окружения или использование значений по умолчанию
-SELENIUM_GRID_URL=${SELENIUM_GRID_URL:-"http://localhost:4444/wd/hub"}
-BROWSER_NAME=${BROWSER_NAME:-"chrome"}
-BROWSER_VERSION=${BROWSER_VERSION:-"124.0"}
+# Проверка наличия переданных параметров
+if [ $# -lt 3 ]; then
+    echo "Использование: $0 <url> <browser_name> <browser_version>"
+    exit 1
+fi
+
+# Присваиваем параметры командной строки переменным
+SELENIUM_GRID_URL=$1
+BROWSER_NAME=$2
+BROWSER_VERSION=$3
 
 # Логирование
-echo "Running tests with the following configuration:"
+echo "Запуск тестов с следующими параметрами:"
 echo "Selenium Grid URL: $SELENIUM_GRID_URL"
-echo "Browser: $BROWSER_NAME"
-echo "Browser Version: $BROWSER_VERSION"
+echo "Браузер: $BROWSER_NAME"
+echo "Версия браузера: $BROWSER_VERSION"
 
-# Запуск Maven тестов с параметрами
+# Запуск Maven тестов с переданными параметрами
 mvn clean test -Dselenium.grid.url=$SELENIUM_GRID_URL -Dbrowser.name=$BROWSER_NAME -Dbrowser.version=$BROWSER_VERSION
